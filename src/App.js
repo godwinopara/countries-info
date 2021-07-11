94b8d1a83ca3aea80369cde9bf8a817bce3ea7f8
Godwin Opara <godwinopara62@yahoo.com>
2021-07-11 19:52:34 +0400

    installed sass and add some css reset

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+import "./pages/Home";
+import "./pages/CountryDetails";
+
+function App() {
+	return (
+		<div>
+			<h1>App</h1>
+		</div>
+	);
+}
+
+export default App;
